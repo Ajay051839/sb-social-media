093b6d732ed98d0f9f180567f12f6922b1394aba
Ajay Patel <ajaypatelkota@gmail.com>
2026-09-04 20:36:47 +0530

    Initial Commit

---
+++ src/main/java/com/media/social/models/SocialProfile.java
@@ -0,0 +1,13 @@
+package com.media.social.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToOne
+    @JoinColumn(name="social_user") //specify the name of foreign key column
+    private SocialUser user;
+}
