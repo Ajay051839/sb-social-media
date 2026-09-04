3c1313cb43560e1c40717c61d3c245bdd5d832b3
Ajay Patel <ajaypatelkota@gmail.com>
2026-09-04 21:16:32 +0530

    Bi-directional mapping between SocialProfile and SocialUser|Not Good

--- src/main/java/com/media/social/models/SocialUser.java
+++ src/main/java/com/media/social/models/SocialUser.java
@@ -1,13 +1,12 @@
 package com.media.social.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+    @OneToOne
+    private SocialProfile socialProfile;
 }
