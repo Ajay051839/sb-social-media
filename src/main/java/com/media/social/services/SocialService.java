5694c74cdf618a72919a7dbab45f2f7571fc8420
Ajay Patel <ajaypatelkota@gmail.com>
2026-09-06 20:53:45 +0530

    Service Added to handle Service Implementation directly

--- src/main/java/com/media/social/services/SocialService.java
+++ src/main/java/com/media/social/services/SocialService.java
@@ -1,4 +1,22 @@
 package com.media.social.services;
 
+import com.media.social.models.SocialUser;
+import com.media.social.repositories.SocialUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SocialService {
+    @Autowired    //Autowiring SocialUserRepository
+    SocialUserRepository socialUserRepository;
+
+    public SocialUser saveUser(SocialUser socialUser) {
+        return socialUserRepository.save(socialUser);
+    }
+
+    public List<SocialUser> getAllUsers() {
+        return socialUserRepository.findAll();
+    }
 }
